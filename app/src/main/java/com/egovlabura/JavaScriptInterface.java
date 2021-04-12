6512f57267100c1642f0d39e2a502efb1ebd3ec4
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-04-12 10:26:32 +0700

    first commit

---
+++ app/src/main/java/com/egovlabura/JavaScriptInterface.java
@@ -0,0 +1,41 @@
+package com.egovlabura;
+
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+public class JavaScriptInterface {
+    Context mContext;
+
+    /** Instantiate the interface and set the context */
+    JavaScriptInterface(Context c) {
+        mContext = c;
+    }
+
+    /** Show a toast from the web page */
+    @JavascriptInterface
+    public void showToast(String toast) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+    }
+
+    public void calculateLocation()
+    {
+        GPSTracker gps = new GPSTracker(mContext);
+
+        // Check if GPS enabled
+        if(gps.canGetLocation()) {
+
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+
+
+            // \n is for new line
+            Toast.makeText(mContext, "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+        } else {
+            // Can't get location.
+            // GPS or network is not enabled.
+            // Ask user to enable GPS/network in settings.
+            gps.showSettingsAlert();
+        }
+    }
+}
---
+++ app/src/main/java/com/egovlabura/MainActivity.java
@@ -0,0 +1,140 @@
+package com.egovlabura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.webkit.ConsoleMessage;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "WV";
+    WebView myWebView;
+    GPSTracker gps;
+    double latitude, longitude;
+    ProgressBar spinner;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+
+        spinner = (ProgressBar)findViewById(R.id.progressBar);
+
+        GPSTracker gps = new GPSTracker(MainActivity.this);
+
+        // Check if GPS enabled
+        if(gps.canGetLocation()) {
+            latitude = gps.getLatitude();
+            longitude = gps.getLongitude();
+
+
+            // \n is for new line
+//            Toast.makeText(MainActivity.this, "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+        } else {
+            // Can't get location.
+            // GPS or network is not enabled.
+            // Ask user to enable GPS/network in settings.
+            gps.showSettingsAlert();
+        }
+
+        myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.addJavascriptInterface(new JavaScriptInterface(this), "Android");
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setAllowFileAccessFromFileURLs(true);
+        myWebView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        myWebView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+
+        myWebView.setWebViewClient(new WebViewClient(){
+            boolean pageStarted = false;
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String urlNewString) {
+                spinner.setVisibility(View.VISIBLE);
+                view.loadUrl(urlNewString);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+//                spinner.setVisibility(View.VISIBLE);
+                pageStarted = true;
+                Log.d("WebView", "onPageStarted " + url);
+                //SHOW LOADING IF IT ISNT ALREADY VISIBLE
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                // do your stuff here
+                if(pageStarted)
+                    spinner.setVisibility(View.GONE);
+                pageStarted = false;
+                view.loadUrl("javascript:startMedia({lat:"+latitude+",lng:"+longitude+"})");
+            }
+        });
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            // Grant permissions for cam
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                Log.d(TAG, "onPermissionRequest");
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @TargetApi(Build.VERSION_CODES.M)
+                    @Override
+                    public void run() {
+                        Log.d(TAG, request.getOrigin().toString());
+                        if(request.getOrigin().toString().equals("https://absensi-ng.labura.go.id/")) {
+                            Log.d(TAG, "GRANTED");
+                            request.grant(request.getResources());
+                        } else {
+                            Log.d(TAG, "DENIED");
+                            request.deny();
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.d(TAG, consoleMessage.message() + " -- From line " +
+                        consoleMessage.lineNumber() + " of " + consoleMessage.sourceId());
+                return true;
+            }
+
+
+        });
+
+        myWebView.loadUrl("https://layanan.labura.go.id");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (myWebView.canGoBack()) {
+                        spinner.setVisibility(View.VISIBLE);
+                        myWebView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
