1ce44b47a2265572d95e8b06edca48316c82b72e
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-09-20 01:07:45 +0700

    add gps background services

---
+++ app/src/main/java/com/egovlabura/GpsService.java
@@ -0,0 +1,34 @@
+package com.egovlabura;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.os.IBinder;
+
+import com.google.android.gms.location.LocationResult;
+
+public class GpsService extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent != null)
+        {
+            String action = intent.getAction();
+            if(action.equals("1"))
+            {
+                LocationResult result = LocationResult.extractResult(intent);
+                if(result != null)
+                {
+                    Location location = result.getLastLocation();
+                    try {
+                        MainActivity.getInstance().showUpdateLocation("Lokasi Anda \nLat : " + location.getLatitude() + "\nLng : " + location.getLongitude());
+                    }catch (Exception e){
+
+                    }
+                }
+            }
+        }
+    }
+}
